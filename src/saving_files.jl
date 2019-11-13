d738c65da2ae30a7545360cce3637d24411a5bbb
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-11-13 11:26:32 +0000

    Snapshot of branch master taken on 2019-11-13T06:26:31.015-05:00 from "https://github.com/UnofficialJuliaMirror/DrWatson.jl-634d3b9d-ee7a-5ddf-bec9-22491ea816e1"

--- src/saving_files.jl
+++ src/saving_files.jl
@@ -49,7 +49,6 @@ function produce_or_load(path::String, c, f;
         end
         file = f(c)
         try
-            mkpath(dirname(s))
             if tag
                 tagsave(s, file; safe = false, gitpath = gitpath)
             else
@@ -85,7 +84,6 @@ contains a key `:gitcommit`, it is not overwritten, unless,
 """
 function tagsave(file, d; safe::Bool = false, gitpath = projectdir(), storepatch = true, force = false, source = nothing)
     d2 = tag!(d, gitpath=gitpath, storepatch=storepatch, force=force, source=source)
-    mkpath(dirname(file))
     if safe
         safesave(file, copy(d2))
     else
@@ -96,7 +94,7 @@ end
 
 
 """
-    @tagsave(file::String, d::Dict; safe = false, gitpath = projectdir(), force = false)
+    @tagsave(file::String, d::Dict; kwargs...)
 Same as [`tagsave`](@ref) but one more field `:script` is added that records
 the local path of the script and line number that called `@tagsave`, see [`@tag!`](@ref).
 """
@@ -143,7 +141,6 @@ See also [`tagsave`](@ref).
 """
 function safesave(f, data)
     recursively_clear_path(f)
-    mkpath(dirname(f))
     wsave(f, data)
 end
 
